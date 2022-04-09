e97f621d66edcb3ccbe75f27f8a57b7bfa2f842f
martinnnachi <mnnachi@gmail.com>
2022-04-09 20:29:56 +0100

    eCommerce-Project: Integrating Shopping template and product list

---
+++ src/app/common/product.ts
@@ -0,0 +1,12 @@
+export class Product {
+  sku: string | undefined;
+  name: string | undefined;
+  description: string | undefined;
+  unitPrice: number | undefined;
+  unitsInStock: number | undefined;
+  imageUrl: string | undefined;
+  active: boolean | undefined;
+  dateCreated: Date | undefined;
+  lastUpdated: Date | undefined;
+
+}
