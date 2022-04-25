45025d0c451d3357e0d6a1b6cf8f5ee91336bc10
martinnnachi <mnnachi@gmail.com>
2022-04-25 18:41:34 +0100

    eCommerce-Project: shopping cart implemented

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -44,7 +44,7 @@ export class CartService {
     this.computeCartTotals()
   }
 
-  private computeCartTotals() {
+  public computeCartTotals() {
     let totalPriceValue: number = 0;
     let totalQuantityValue: number = 0;
 
@@ -73,5 +73,30 @@ export class CartService {
 
     console.log(`totalPrice: ${totalPriceValue.toFixed(2)}, totalQuantity: ${totalQuantityValue}`)
     console.log('-----')
+  }
+
+  decrementQuantity(theCartItem: CartItem) {
+    theCartItem.quantity--
+
+    if (theCartItem.quantity === 0) {
+      this.remove(theCartItem)
+    } else {
+      this.computeCartTotals()
+    }
+
+  }
+
+  remove(theCartItem: CartItem) {
+    // get index of item in the array
+    const itemIndex = this.cartItems.findIndex(tempCartItem => tempCartItem.id === theCartItem.id)
+
+    // if found, remove the item from the array at the given index
+    if (itemIndex > -1) {
+      this.cartItems.splice(itemIndex, 1)
+
+      this.computeCartTotals()
+    }
+
+
   }
 }
